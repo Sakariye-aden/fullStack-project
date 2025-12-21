d3281bff23355f1c949a693732d3176fd514b52a
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-12-21 15:10:29 +0300

    added store

--- frontend/src/lib/Store/AuthStore.js
+++ frontend/src/lib/Store/AuthStore.js
@@ -1,9 +1,39 @@
 import {create } from 'zustand'
 import {persist } from 'zustand/middleware'
 // store use get 
-const useAuthstore = create({
-    
+const useAuthstore = create(
      
-})
+    persist(
+        (set, get)=>(
+        {
+           user : null,
+           token :null,
+           isAuthenticted :false,
+
+           setUser : ()=>set((userData, token)=>({
+               user : userData,
+               token,
+               isAuthenticted: true
+             })),
+
+           clearAuth : ()=>set({
+              user : null,
+              token:null,
+              isAuthenticted: false
+           })  ,
+
+           getToken : get().token
+        },
+        {
+            name:"testAuth",
+            partialize : (state)=>({
+                user:state.user,
+                token : state.token,
+                isAuthenticted: state.isAuthenticted
+            })
+        }
+ )
+)
+);
 
 export default useAuthstore

491002e9219d8d30cb7d09b1bb35583be57ca41c
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-12-23 07:09:20 +0300

    added interceptors in api

--- frontend/src/lib/Api/ApiClient.js
+++ frontend/src/lib/Api/ApiClient.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import useAuthStore from '../Store/AuthStore'
 
 const Api_url = 'http://localhost:5000/api'
 
@@ -9,5 +10,14 @@ const api = axios.create({
   }
 })
 
+api.interceptors.request.use((config)=>{
+   
+     const token  = useAuthStore.getState().token;
+      if(token){
+        config.headers.Authorization = `Bearer ${token}`
+      }
+
+      return config
+})
 
 export default api

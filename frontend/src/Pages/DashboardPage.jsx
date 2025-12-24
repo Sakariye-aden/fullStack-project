17ee29b3180a172a8f1404f200386b82fde9720c
Sakariye-aden <sakariyeadenhaji@gmail.com>
2025-12-24 17:41:08 +0300

    updated in dashboardpage

--- frontend/src/Pages/DashboardPage.jsx
+++ frontend/src/Pages/DashboardPage.jsx
@@ -13,60 +13,61 @@ import { useState } from 'react'
 const DashboardPage = () => {
 
   const { user } = useAuthstore()
-  const [isOpen , setisOpen]= useState(false)
+  const [isOpen , setisOpen]= useState(true)
 
   return (
     <div className='min-h-screen bg-gray-50'>
         {/* labtop designn */}
-         <div  className='hidden md:grid grid-cols-5  min-h-screen'>
+         <div  className='hidden md:flex   min-h-screen'>
            {/* left bar */}
            <div className='border-r bg-card'>
+                {/* nav */}
                <div className='bg-card border-b  p-2'>
                   <span className='font-medium text-2xl text-gray-800'>Finance</span> 
                   { isOpen && <span className='pl-2 text-xl text-gray-500'>Tracker</span>}
                </div>
+               
                <div className='h-110 flex flex-col gap-6 px-2 py-6 border-b'>
                  <Link to="/dashboard" className='flex items-center space-x-2'>
                    <LayoutDashboard  className='w-6 h-6' />
-                    <span>Dashboard</span>
+                    {isOpen && <span>Dashboard</span> }
                  </Link>
                  <Link to="/dashboard/transactions" className='flex items-center space-x-2'>
                     <ArrowRightLeft />
-                    <span>Transactions</span>
+                    {isOpen && <span>Transactions</span>}
                  </Link>
                  <Link to="/dashboard/report" className='flex items-center space-x-2'>
                       <ChartColumn />
-                    <span>Reports</span>
+                    {isOpen &&  <span>Reports</span> }
                  </Link>
                  {
                    user.role == 'admin' && (
                     <Link to="/dashboard/admin" className='flex items-center space-x-2'>
                         <UserStar />
-                      <span>Admin</span>
+                       {isOpen && <span>Admin</span> }
                     </Link>
                   )
                  }
               </div>
                 <div className='flex p-2'>
-                   {/* <div className='h-11 w-10 flex justify-center text-white text-3xl font-medium rounded-full bg-blue-600 text-center'>{user.name[0]}</div>   */}
-                     <Avatar>
+                     <Avatar className='h-10 w-10'>
                       <AvatarImage src="https://img.freepik.com/premium-vector/man-professional-business-casual-young-avatar-icon-illustration_1277826-629.jpg" />
                       <AvatarFallback>CN</AvatarFallback>
                      </Avatar>
                    <div className='flex flex-col overflow-hidden'>
-                     <span className='font-medium text-lg'>{user.name}</span>
-                     {/* <span>online</span> */}
+                     {isOpen && <span className='font-medium text-lg'>{user.name}</span>}
+                    
                    </div>     
                 </div>
                  <div className='p-2 '>
-                    <Button>
+                    <Button >
                       <LogOut />
                       Logout 
                     </Button>
                  </div>
            </div> 
            {/* right bar  */}
-           <div className='col-span-4 '>
+           <div className='flex-1'>
               
               <div className='bg-card flex items-center justify-between p-2 border-b'>
                  <div className='flex space-x-2'>
